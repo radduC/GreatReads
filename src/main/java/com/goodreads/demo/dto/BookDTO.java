75fe9aaaeb0c910bf019229dccae54863cec19d6
radu.costache <radu.costache@cgi.com>
2023-08-31 16:03:43 +0300

    Implemented get all books by genre to use BookDTO

--- src/main/java/com/goodreads/demo/dto/BookDTO.java
+++ src/main/java/com/goodreads/demo/dto/BookDTO.java
@@ -1,7 +1,6 @@
 package com.goodreads.demo.dto;
 
 import com.goodreads.demo.entities.Book;
-import com.goodreads.demo.entities.User;
 
 import java.time.LocalDate;
 
@@ -12,10 +11,6 @@ public record BookDTO(
         String description,
         LocalDate publishedDate
 ) {
-    public BookDTO(String title, User author, String genre, String description, LocalDate publishedDate) {
-        this(title, author.getFirstName() + " " + author.getLastName(), genre, description, publishedDate);
-    }
-
     public BookDTO(Book book) {
         this(book.getTitle(), book.getAuthor().getFirstName() + " " + book.getAuthor().getLastName(),
                 book.getGenre(), book.getDescription(), book.getPublishedDate());
